11217b23f0de268bc3ed858826637e0155b76695
subodh-baniya <baniyasubodh07@gmail.com>
2026-05-09 02:40:18 +0545

    added the route for gemini api

--- Backend/src/Routes/report.route.js
+++ Backend/src/Routes/report.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-
+import { getGeneSummary } from "../Controller/gemini.controller.js";
 import { getGeneReport } from "../Controller/report.controller.js";
 
 const router = express.Router();
 router.get("/report/:gene", getGeneReport);
+router.get("/gemini-summary/:gene", getGeneSummary);
 
 export default router;
